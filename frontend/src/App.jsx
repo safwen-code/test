d1d24e69fd97c4be8233a52d8f3c0784885edf82
safwen-code <safwendjebbi1234@gmail.com>
2026-03-23 23:55:00 +0100

    update api render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-const API = 'http://localhost:5000'
+const API = 'https://test-w070.onrender.com'
 
 function App() {
   const [products, setProducts] = useState([])
